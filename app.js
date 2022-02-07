281fcd64089fb4f3c3a272e67323de712faf4d91
srikar chavali <chavalisrikar@gmail.com>
2022-02-07 01:08:27 +0000

    seconf commit

--- app.js
+++ app.js
@@ -2,11 +2,99 @@
 // npm init
 // npm i --save-dev jest
 
-const add = (num1, num2) => num1+num2;
-const myArray = ['dan', 'tom', 'ben']
+// const add = (num1, num2) => num1+num2;
+// const myArray = ['dan', 'tom', 'ben']
 
-module.exports = {
-    add,
-    myArray
-}
+// module.exports = {
+//     add,
+//     myArray
+// }
+
+notNull = () =>{
+    5;
+  }
+  
+ isTrue = () =>{
+    true;
+  }
+  
+  function object(val1, val2) {
+    return {
+      val1: val1,
+      val2: val2,
+    };
+  }
+  
+  const array = (input) => {
+    let output = [];
+    for (let i = 0; i < input.length; i++) {
+      if (input[i].length >= 6) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  };
+  
+
+  function numToStr(num) {
+    return String(num);
+  }
+  
+  function planet(num) {
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Jupyter"];
+    return planets[num - 1];
+  }
+  
+  function students(reg) {
+    present = 0;
+    for (const i in reg) {
+      if (reg[i]) {
+        present += 1;
+      }
+    }
+    return present;
+  }
+  
+  function sqrDigits(num) {
+    num = String(num);
+    num = num.split("");
+    for (let i = 0; i < num.length; i++) {
+      num[i] = Number(num[i]);
+      num[i] = num[i] ** 2;
+      num[i] = String(num[i]);
+    }
+    returnValue = "";
+    for (let i = 0; i < num.length; i++) {
+      returnValue = returnValue + num[i];
+    }
+    return (
+        Number(returnValue),
+        console.log(returnValue)
+    )
+  }
+  sqrDigits(25)
+//   const centuryOf = (year) => {
+//     return floor(year / 100);
+//   };
+  
+//   const binaryArrayToInt = (input) => {
+//     for (let i = 0; i < input.length; i++) {
+//       input[i] = String(input[i]);
+//     }
+//     input = input.join("");
+//     return parseInt(input, 2);
+//   };
+  
+  module.exports = {
+    notNull,
+    isTrue,
+    object,
+    array,
+    numToStr,
+    planet,
+    students,
+    sqrDigits,
+    // centuryOf,
+    // binaryArrayToInt,
+  };
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -7,18 +7,66 @@
 // Tests can be grouped together 
 // describe(')
 
-const app = require("../app.js")
+const app = require("../app")
 
-test('should return 5 when 2 and 3 are passed', () => {
+// test('should return 5 when 2 and 3 are passed', () => {
 
-    //assertions
-    expect(app.add(2,3)).toEqual(5);
-});
+//     //assertions
+//     expect(app.add(2,3)).toEqual(5);
+// });
 
-// test and it do the same thing
-it('should contain tom', () => {
+// // test and it do the same thing
+// it('should contain tom', () => {
 
-    //assertions
-    expect(app.myArray).toContain('tom');
-})
+//     assertions
+//     expect(app.myArray).toContain('tom');
+// })
 
+test("returns a non null value", () => {
+    expect(app.notNull()).not.toBeNull;
+  });
+
+  test("checks if a value is True", () => {
+    expect(app.isTrue()).toBeTruthy;
+  });
+
+  test("Object's  two expected values (hello, world)", () => {
+    expect(app.object("hello", "world")).toHaveProperty("val1", "hello");
+    expect(app.object("hello", "world")).toHaveProperty("val2", "world");
+  });
+
+  test("return array items with 6 or more charecters", () => {
+    expect(app.array(['hi', 'adwferg', 'chgxdytyu'])).toHaveLength(6);
+  });
+
+  test(`5 becomes "5"`, () => {
+    expect(app.numToStr(5)).toBe("5");
+  });
+
+  test("third planet is earth", () => {
+    expect(app.planet(3)).toBe("Earth");
+  });
+
+  test("there are three students in class", () => {
+    expect(app.students([true, true, true, false, false])).toBe(3);
+  });
+
+  test("sqrDigits 25 = 425", () => {
+    expect(app.sqrDigits(25)).toBe(425);
+  });
+
+  // test('Array of names only contains "Stephanie"', () => {
+  //   success = ["Stephanie"];
+  //   expect(
+  //     app.onlyItemsOfnLength(["cath", "jess", "gorb", "Stephanie"])
+  //   ).toEqual(success);
+  // });
+
+
+  // test("1705 was in the 17th century", () => {
+  //   expect(app.centuryOf(1705)).toBe(17);
+  // });
+
+  // test("[0,1,0,0] translates to 4", () => {
+  //   expect(app.binaryArrayToInt([0, 1, 0, 0])).toBe(4);
+  // });
